3ab0e1d236c259ad0f903b200312d73172af7008
0and4 <iwonchu215@gmail.com>
2022-05-26 16:29:34 +0900

    1. 마트

--- MartTest.java
+++ MartTest.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 class Mart{
 	Scanner sc = new Scanner(System.in);
 	private int purchase;
-	private boolean member;
-	private String m;
+	private String member;
 	String arr[]= {"member1234","member5678"};
 	public Mart(int purchase) {
 		this.purchase=purchase;
 	}
-	public Mart(boolean member, int purchase) {
+	public Mart(String member, int purchase) {
 		this.member=member;
 		this.purchase=purchase;
+		isMember(member);
 	}
 	public int getPurchase() {
 		return purchase;
@@ -19,39 +19,38 @@ class Mart{
 	public void setPurchase(int purchase) {
 		this.purchase=purchase;
 	}
-	boolean isMember() {
+	boolean isMember(String member) {
+		boolean m=false;
 		for(int i=0;i<arr.length;i++) {
-			if (m==arr[i]) {
-				member=true;
+			if (arr[i].equals(member)) {
+				m=true;
 			}
-			else member=false;
 		}
-		return member;
+		return m;
 	}
-	public void getMember(boolean member) {
+	public void getMember(String member) {
 		this.member=member;
 	}
 	public int Sale() {
-		if(purchase<=1000000 && purchase>=0) {
-			if(purchase>=10000) {
-				purchase-=purchase*0.05;
-			}
-			else if(purchase>=50000) {
-				purchase-=purchase*0.1;
-			}
-			else if(purchase>=100000) {
-				purchase-=purchase*0.2;
-			}
-			if(isMember()==true) {
-				purchase-=purchase*0.1;
-				System.out.print("회원 ID : "+m+"의 ");
-			}
-		}
-		else if(purchase>1000000 || purchase<0) {
+		double sale=0;
+		int result = 0;
+		if(purchase>1000000 || purchase<0) {
 			System.out.println("0원 이상 1,000,000원 이하의 금액을 입력하십시오.");
 		}
-		System.out.println("최종 결제액은 "+purchase+"입니다.");
-		return purchase;
+		else {
+			if(purchase>=10000 && purchase<50000) { sale=0.95; }
+			else if(purchase>=50000 && purchase<100000) { sale=0.9; }
+			else if(purchase>=100000) { sale=0.8; }
+		}
+		if(isMember(member)==true) {
+			result=(int)(purchase*(sale-0.1));
+			System.out.print("회원 ID : "+member+"의 ");
+		}
+		else {
+			result=(int)(purchase*sale);
+		}
+		System.out.println("최종 결제액은 "+result+"입니다.");
+		return result;
 	}
 }
 public class MartTest {
@@ -65,9 +64,9 @@ public class MartTest {
 		
 		Mart B;
 		System.out.print("회원번호와 금액을 입력하세요 : ");
-		boolean member=B.isMember();
+		String member=sc.next();
 		purchase=sc.nextInt();
 		B=new Mart(member,purchase);
-		//회원번호를 boolean형으로 어떻게 입력받아야 하는지 이해하지 못했습니다
+		B.Sale();
 	}
 }
